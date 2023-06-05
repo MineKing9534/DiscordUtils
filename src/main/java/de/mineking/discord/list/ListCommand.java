3e9fc02d4d695f8a85c462e7e70053eb6a9c74e9
MineKing <mineking9534@gmail.com>
2023-06-05 12:58:33 +0200

    Improve ListCommand

--- src/main/java/de/mineking/discord/list/ListCommand.java
+++ src/main/java/de/mineking/discord/list/ListCommand.java
@@ -10,15 +10,16 @@ import net.dv8tion.jda.api.interactions.commands.OptionType;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ListCommand<C extends ContextBase, E extends ListEntry, T extends Listable<E>> extends BaseCommand<C> {
-	public final ListProvider<T, E> handler;
+	public final ListProvider<C, E, T> handler;
 
-	public ListCommand(CommandPermission permission, ListProvider<T, E> handler) {
+	public ListCommand(CommandPermission permission, ListProvider<C, E ,T> handler) {
 		this(permission, handler, Collections.emptyList());
 	}
 
-	public ListCommand(CommandPermission permission, ListProvider<T, E> handler, List<Option> options) {
+	public ListCommand(CommandPermission permission, ListProvider<C, E, T> handler, List<Option> options) {
 		options.forEach(this::addOption);
 		addOption(new Option(OptionType.INTEGER, "page", "page").localizeCustom().range(1, null));
 
@@ -30,7 +31,7 @@ public class ListCommand<C extends ContextBase, E extends ListEntry, T extends L
 	public void performCommand(C context, GenericCommandInteractionEvent event) {
 		event.deferReply(true).queue();
 
-		var listable = handler.getObject(event);
+		var listable = handler.getObject(context, event.getOptions().stream().collect(Collectors.toMap(OptionMapping::getName, OptionMapping::getAsString)));
 
 		if(listable == null) {
 			return;
--- src/main/java/de/mineking/discord/list/ListProvider.java
+++ src/main/java/de/mineking/discord/list/ListProvider.java
@@ -1,17 +1,9 @@
 package de.mineking.discord.list;
 
-import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.User;
-import net.dv8tion.jda.api.events.interaction.command.GenericCommandInteractionEvent;
-import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import de.mineking.discord.commands.ContextBase;
 
 import java.util.Map;
-import java.util.stream.Collectors;
 
-public interface ListProvider<T extends Listable<E>, E extends ListEntry> {
-	T getObject(User user, Guild guild, Map<String, String> params);
-
-	default T getObject(GenericCommandInteractionEvent event) {
-		return getObject(event.getUser(), event.getGuild(), event.getOptions().stream().collect(Collectors.toMap(OptionMapping::getName, OptionMapping::getAsString)));
-	}
+public interface ListProvider<C extends ContextBase, E extends ListEntry, T extends Listable<E>> {
+	T getObject(C context, Map<String, String> params);
 }
