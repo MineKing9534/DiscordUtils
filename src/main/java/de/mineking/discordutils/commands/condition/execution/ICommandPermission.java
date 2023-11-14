5967520bf73f1d3c76b76b95b1215de5a7fb7604
MineKing <mineking9534@gmail.com>
2023-11-14 19:21:47 +0100

    Make ICommandPermission extend IRegistrationCondition

--- src/main/java/de/mineking/discordutils/commands/condition/execution/ICommandPermission.java
+++ src/main/java/de/mineking/discordutils/commands/condition/execution/ICommandPermission.java
@@ -1,12 +1,16 @@
 package de.mineking.discordutils.commands.condition.execution;
 
 import de.mineking.discordutils.commands.CommandManager;
+import de.mineking.discordutils.commands.condition.registration.IRegistrationCondition;
 import de.mineking.discordutils.commands.context.ContextBase;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.interaction.command.GenericCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.DiscordLocale;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public interface ICommandPermission<C extends ContextBase<GenericCommandInteractionEvent>> extends IExecutionCondition<C> {
+public interface ICommandPermission<C extends ContextBase<GenericCommandInteractionEvent>> extends IExecutionCondition<C>, IRegistrationCondition<C> {
 	boolean isPermitted(CommandManager<C, ?> manager, Member member);
 
 	void handleUnpermitted(CommandManager<C, ?> manager, C context);
@@ -18,6 +22,17 @@ public interface ICommandPermission<C extends ContextBase<GenericCommandInteract
 			return false;
 		}
 
+		return true;
+	}
+
+	@NotNull
+	@Override
+	default String format(@NotNull DiscordLocale locale) {
+		return toString();
+	}
+
+	@Override
+	default boolean shouldRegister(@NotNull CommandManager<C, ?> manager, @Nullable Guild guild) {
 		return true;
 	}
 }
