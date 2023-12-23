abea6b4df83cdaa63ef3af89851227ed66ee5c38
MineKing <mineking9534@gmail.com>
2023-12-23 22:07:50 +0100

    Fix conditions for commands without method

--- src/main/java/de/mineking/discordutils/commands/AnnotatedCommand.java
+++ src/main/java/de/mineking/discordutils/commands/AnnotatedCommand.java
@@ -103,15 +103,15 @@ public class AnnotatedCommand<T, C extends ICommandContext, A extends IAutocompl
 
 				manager.getParser(p).ifPresent(op -> op.registerOption(this, buildOption(o, p, generic, name, autocomplete.get(o.id().isEmpty() ? name : o.id()), choices.get(o.id().isEmpty() ? name : o.id())), p));
 			}
+		}
 
-			var tInstance = instance.apply(null).orElse(null);
-			for(var f : clazz.getFields()) {
-				try {
-					if(IExecutionCondition.class.isAssignableFrom(f.getType())) condition = getCondition().and((IExecutionCondition<C>) f.get(tInstance));
-					else if(IRegistrationCondition.class.isAssignableFrom(f.getType())) registration = getRegistration().and((IRegistrationCondition<C>) f.get(tInstance));
-				} catch(Exception e) {
-					CommandManager.logger.error("Failed to read condition field '{}' for command '{}'", f.getName(), name, e);
-				}
+		var tInstance = instance.apply(null).orElse(null);
+		for(var f : clazz.getFields()) {
+			try {
+				if(IExecutionCondition.class.isAssignableFrom(f.getType())) condition = getCondition().and((IExecutionCondition<C>) f.get(tInstance));
+				else if(IRegistrationCondition.class.isAssignableFrom(f.getType())) registration = getRegistration().and((IRegistrationCondition<C>) f.get(tInstance));
+			} catch(Exception e) {
+				CommandManager.logger.error("Failed to read condition field '{}' for command '{}'", f.getName(), name, e);
 			}
 		}
 
