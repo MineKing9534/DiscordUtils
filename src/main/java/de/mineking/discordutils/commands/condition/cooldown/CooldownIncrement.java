71c53137f051e55bb22452d4927fc8eb80785ad2
MineKing <69981672+MineKing9534@users.noreply.github.com>
2024-04-03 14:10:33 +0200

    Make cooldown more customizable (#10)

---
+++ src/main/java/de/mineking/discordutils/commands/condition/cooldown/CooldownIncrement.java
@@ -0,0 +1,11 @@
+package de.mineking.discordutils.commands.condition.cooldown;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CooldownIncrement {
+}
